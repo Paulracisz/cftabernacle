ab79f78232b5a221828251e65e65fc1f3eddb6ca
Paul <paulieracisz@gmail.com>
2020-09-18 14:41:49 -0400

    Updates

---
+++ precache-manifest.b3e2a24fd940512942f1db8a2d560688.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460e4883e8318748470e2244095e6d45",
+    "url": "/cftabernacle/index.html"
+  },
+  {
+    "revision": "4c6c419162f71b4a7b0c",
+    "url": "/cftabernacle/static/css/main.35f9392c.chunk.css"
+  },
+  {
+    "revision": "2a9f63c6a9da688774f4",
+    "url": "/cftabernacle/static/js/2.69b9274e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cftabernacle/static/js/2.69b9274e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6c419162f71b4a7b0c",
+    "url": "/cftabernacle/static/js/main.cf131c6f.chunk.js"
+  },
+  {
+    "revision": "135d71958a7315c11868",
+    "url": "/cftabernacle/static/js/runtime-main.01278397.js"
+  },
+  {
+    "revision": "b9fc359fd9c96aa54985d75ab2c1e211",
+    "url": "/cftabernacle/static/media/bg.b9fc359f.jpg"
+  },
+  {
+    "revision": "e05492229b5c0e51eabae51180f4ed7b",
+    "url": "/cftabernacle/static/media/logo3.e0549222.png"
+  },
+  {
+    "revision": "8ab78cdeda10030d1949ffc81146d001",
+    "url": "/cftabernacle/static/media/profile1.8ab78cde.jpeg"
+  },
+  {
+    "revision": "b90f6d2e2824019d64e91ac31b232ae4",
+    "url": "/cftabernacle/static/media/profile3.b90f6d2e.jpeg"
+  }
+]);
