ea8e18925d20a13daa3e43d73e04218f41b2d2ad
Paul <paulieracisz@gmail.com>
2020-12-11 19:36:22 -0500

    I spelled autoPlay wrong -_-

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -6,7 +6,7 @@ import "../css/About.css"
 export default class MyCarousel extends Component {
   render() {
     return (
-<video id="videoBox" width="440" height="440" autPlay controls>
+<video id="videoBox" width="440" height="440" autoPlay controls>
 <source src={Movie} type="video/mp4"/>
 Your browser does not support the video tag.
 </video>
